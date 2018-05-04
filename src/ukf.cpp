d286e27f0fffc042cad0228d6937f1abdb872a26
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2018-05-04 18:24:05 +0200

    Calculated sigma points with augmented X and P

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
     P_ = MatrixXd(5, 5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 30;
+    std_a_ = 0.3; // 30? Really?
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 30;
+    std_yawdd_ = 0.3; // 30? Really?
 
     //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
     // Laser measurement noise standard deviation position1 in m
@@ -56,8 +56,8 @@ UKF::UKF() {
     */
 
     n_x_ = 5;
-    n_aug_ = 5;
-    lambda_ = 1;
+    n_aug_ = 7;
+    lambda_ = 3 - n_x_;
 }
 
 UKF::~UKF() {}
@@ -73,6 +73,13 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     Complete this function! Make sure you switch between lidar and radar
     measurements.
     */
+    if(meas_package.sensor_type_ == MeasurementPackage::LASER) {
+        // LASER
+    }
+    if(meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+        // LASER
+    }
+
 }
 
 /**
@@ -87,6 +94,33 @@ void UKF::Prediction(double delta_t) {
     Complete this function! Estimate the object's location. Modify the state
     vector, x_. Predict sigma points, the state, and the state covariance matrix.
     */
+
+    // augment X
+    VectorXd x_aug = VectorXd(n_aug_);
+    x_aug.head(5) = x_;
+    x_aug(5) = 0;
+    x_aug(6) = 0;
+
+    MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+    P_aug.fill(0.0);
+    P_aug.topLeftCorner(5,5) = P_;
+    // adding the Q matrix to the augmented P
+    P_aug(5,5) = std_a_*std_a_;
+    P_aug(6,6) = std_yawdd_*std_yawdd_;
+
+
+    MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+    MatrixXd A = P_aug.llt().matrixL();
+
+    // Calculating sigma points
+    Xsig_aug.col(0) = x_;
+
+    for(int i=0; i<n_x_; i++) {
+        Xsig_aug.col(i+1) = x_ + std::sqrt(lambda_ + n_aug_) * A.col(i);
+        Xsig_aug.col(i+1+n_aug_) = x_ - std::sqrt(lambda_ + n_aug_) * A.col(i);
+    }
+
 }
 
 /**
